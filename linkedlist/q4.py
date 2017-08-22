2aba126c03ecce831ba165add16ad5b12d3e7b82
resolvewang <1796246076@qq.com>
2017-08-22 09:04:33 +0800

    添加双链表反转

--- linkedlist/q4.py
+++ linkedlist/q4.py
@@ -5,14 +5,13 @@
 """
 
 
-from linkedlist.toolcls import PrintMixin, Node
+from linkedlist.toolcls import PrintMixin, Node, DoubleNode
 
 
 class RevertList(PrintMixin):
     @classmethod
     def revert_linked_list(cls, head):
         pre = None
-        next = None
 
         while head is not None:
             next = head.next
@@ -22,6 +21,17 @@ class RevertList(PrintMixin):
 
         return pre
 
+    @classmethod
+    def revert_double_linked_list(cls, head):
+        pre = None
+        while head is not None:
+            next = head.next
+            head.next = pre
+            head.pre = next
+            pre = head
+            head = next
+        return pre
+
 
 if __name__ == '__main__':
     node = Node(1)
@@ -29,3 +39,18 @@ if __name__ == '__main__':
     node.next.next = Node(3)
     revert_node = RevertList.revert_linked_list(node)
     RevertList.print_list(revert_node)
+
+    node2 = DoubleNode(1)
+    node2.next = DoubleNode(2)
+    node2.next.pre = node2
+    node2.next.next = DoubleNode(3)
+    node2.next.next.pre = node2.next
+    node2.next.next.next = DoubleNode(4)
+    node2.next.next.next.pre = node2.next.next
+    node2.next.next.next.next = DoubleNode(5)
+    node2.next.next.next.next.pre = node2.next.next.next
+    node2.next.next.next.next.next = DoubleNode(6)
+    node2.next.next.next.next.next.pre = node2.next.next.next
+
+    revert_double_node = RevertList.revert_double_linked_list(node2)
+    RevertList.print_list(revert_double_node)
--- linkedlist/toolcls.py
+++ linkedlist/toolcls.py
@@ -4,6 +4,13 @@ class Node:
         self.next = None
 
 
+class DoubleNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.pre = None
+
+
 class PrintMixin:
     @staticmethod
     def print_list(head):
