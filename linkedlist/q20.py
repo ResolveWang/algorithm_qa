b2d668965333068228ce1ccb2289864cbfcdf891
resolvewang <1796246076@qq.com>
2017-09-03 15:52:33 +0800

    按照左右半区的方式重新组合单链表

--- linkedlist/q20.py
+++ linkedlist/q20.py
@@ -52,6 +52,21 @@ class RefactorList(PrintMixin):
 if __name__ == '__main__':
     head = Node(1)
     head.next = Node(2)
+    RefactorList.print_list(RefactorList.refactor(head))
+
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    RefactorList.print_list(RefactorList.refactor(head))
+
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    RefactorList.print_list(RefactorList.refactor(head))
+
+    head = Node(1)
+    head.next = Node(2)
     head.next.next = Node(3)
     head.next.next.next = Node(4)
     head.next.next.next.next = Node(5)
