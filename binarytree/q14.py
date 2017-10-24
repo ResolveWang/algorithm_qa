9df1a801223d069dcf1ae376d058c0499dc08fbe
resolvewang <1796246076@qq.com>
2017-10-24 20:50:35 +0800

    根据后序数组重建搜索二叉树

--- binarytree/q14.py
+++ binarytree/q14.py
@@ -7,6 +7,7 @@
 
 from binarytree.toolcls import Node
 
+from binarytree.q3 import PrintTree
 
 class ISBST:
     @classmethod
@@ -24,7 +25,7 @@ class ISBST:
         more = end
         for i in range(start, end):
             if arr[i] < arr[end]:
-                less += 1
+                less = i
             else:
                 if more == end:
                     more = i
@@ -35,9 +36,38 @@ class ISBST:
         if more - less != 1:
             return False
 
-        return cls.is_bst_tree_detail(arr, 0, less) and cls.is_bst_tree_detail(arr, more, end-1)
+        return cls.is_bst_tree_detail(arr, start, less) and cls.is_bst_tree_detail(arr, more, end-1)
+
+    @classmethod
+    def recontruct_bst(cls, arr):
+        if arr is None:
+            return None
+        return cls.recontruct_bst_detail(arr, 0, len(arr) - 1)
+
+    @classmethod
+    def recontruct_bst_detail(cls, arr, start, end):
+        if start > end:
+            return None
+
+        less = -1
+        more = end
+
+        cur_node = Node(arr[end])
+
+        for i in range(start, end):
+            if arr[i] < arr[end]:
+                less = i
+            else:
+                more = i if more == end else more
+
+        cur_node.left = cls.recontruct_bst_detail(arr, start, less)
+        cur_node.right = cls.recontruct_bst_detail(arr, more, end-1)
+        return cur_node
 
 
 if __name__ == '__main__':
     my_arr = [2, 1, 3, 6, 5, 7, 4]
     print(ISBST.is_bst_tree(my_arr))
+    from binarytree.q1 import RecursiveVisit
+    RecursiveVisit.visit_in_first_order(ISBST.recontruct_bst(my_arr))
+    PrintTree.print_tree(ISBST.recontruct_bst(my_arr))
