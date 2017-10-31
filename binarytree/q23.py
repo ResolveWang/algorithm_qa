7f8d64cb957b346315e6a9bb42e7d47219b82e02
resolvewang <1796246076@qq.com>
2017-10-31 20:27:09 +0800

    统计和生成所有不同的二叉树

--- binarytree/q23.py
+++ binarytree/q23.py
@@ -4,7 +4,16 @@
 
 例如，N=-1时，代表空树结构，返回1；N=2时，满足中序遍历为{1， 2}的二叉树结构只有两种结构，所以
 结果返回为2
+
+进阶：N的含义不变，假设可能的二叉树结构有M种，请返回M个二叉树的头结点，每一颗二叉树代表一种可能
+的结构。
 """
+
+from itertools import product
+from copy import deepcopy
+
+from binarytree.toolcls import Node
+from binarytree.q3 import PrintTree
 
 
 class TreeFromInt:
@@ -27,7 +36,47 @@ class TreeFromInt:
             return cls.get_total_num(n-1)
         return cls.get_total_num(i-1) * cls.get_total_num(n-i)
 
+    @classmethod
+    def get_roots(cls, n):
+        return cls.get_root(1, n)
+
+    @classmethod
+    def get_root(cls, start, end):
+        res = list()
+        if start > end:
+            res.append(None)
+
+        for i in range(start, end+1):
+            node = Node(i)
+            left_nodes = cls.get_root(start, i-1)
+            right_nodes = cls.get_root(i+1, end)
+            r = product(left_nodes, right_nodes)
+            for left, right in r:
+                node.left = left
+                node.right = right
+                res.append(deepcopy(node))
+            #
+            # for left in left_nodes:
+            #     for right in right_nodes:
+            #         node.left = left
+            #         node.right = right
+            #         res.append(cls.clone_tree(node))
+
+        return res
+
+    # @classmethod
+    # def clone_tree(cls, node):
+    #     if node is None:
+    #         return
+    #     res = Node(node.value)
+    #     res.left = cls.clone_tree(node.left)
+    #     res.right = cls.clone_tree(node.right)
+    #     return res
+
 
 if __name__ == '__main__':
     n = 4
     print(TreeFromInt.get_total_num(n))
+    rs = TreeFromInt.get_roots(n)
+    for r in rs:
+        PrintTree.print_tree(r)
