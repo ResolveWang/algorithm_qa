b9ee1c756171bb962bee74427a560fdd3debd9fa
resolvewang <1796246076@qq.com>
2017-11-06 19:43:32 +0800

    添加原问题空间压缩方法

--- dp/q3.py
+++ dp/q3.py
@@ -12,7 +12,7 @@ import sys
 class CoinsCounter:
     @classmethod
     def min_coins_count_1(cls, arr, aim):
-        if not arr or aim < 0:
+        if not arr or aim < 0 or len(arr) == 0:
             return -1
 
         n = len(arr)
@@ -32,6 +32,31 @@ class CoinsCounter:
 
         return dp[n-1][aim] if dp[n-1][aim] != max_val else -1
 
+    @classmethod
+    def min_coins_count_2(cls, arr, aim):
+        if not arr or aim < 0 or len(arr) == 0:
+            return -1
+
+        n = len(arr)
+        max_val = sys.maxsize
+        dp = [0 for _ in range(aim+1)]
+        for i in range(1, aim+1):
+            dp[i] = max_val
+            if arr[0] <= i and dp[i-arr[0]] != max_val:
+                dp[i] = dp[i-arr[0]] + 1
+
+        for i in range(1, n):
+            for j in range(1, aim+1):
+                left = max_val
+                if arr[i] <= j and dp[j-arr[i]] != max_val:
+                    left = dp[j-arr[i]] + 1
+                dp[j] = min([left, dp[j]])
+
+        return dp[aim] if dp[aim] != max_val else -1
+
 
 if __name__ == '__main__':
-    print(CoinsCounter.min_coins_count_1([5, 2, 3], 20))
+    print(CoinsCounter.min_coins_count_1([100, 20, 5, 10, 2, 50, 1], 17019))
+    print(CoinsCounter.min_coins_count_2([100, 20, 5, 10, 2, 50, 1], 17019))
+
+    print(CoinsCounter.min_coins_count_1([10, 100, 2, 5, 5, 5, 10, 1, 1, 1, 2, 100], 223))
