34915c1eae5a2b40050a04b4d2020575afa94f45
resolvewang <1796246076@qq.com>
2017-11-14 21:11:36 +0800

    最小编辑代价

--- dp/q9.py
+++ dp/q9.py
@@ -14,9 +14,9 @@ str1='abc',str2='abc'，ic=5,dc=3,rc=2
 
 class LowestEditCost:
     @classmethod
-    def get_lowes_edit_cost_way_1(cls, str1, str2, ic, dc, rc):
+    def get_lowest_edit_cost_way_1(cls, str1, str2, ic, dc, rc):
         if str1 is None or str2 is None:
-            return
+            return 0
 
         len1 = len(str1)
         len2 = len(str2)
@@ -40,20 +40,55 @@ class LowestEditCost:
 
         return dp[len1][len2]
 
+    @classmethod
+    def get_lowest_edit_cost_way_2(cls, str1, str2, ic, dc, rc):
+        if str1 is None or str2 is None:
+            return 0
+
+        longs = str1 if len(str1) >= len(str2) else str2
+        shorts = str1 if len(str1) < len(str2) else str2
+        if len(str1) < len(str2):
+            temp = ic
+            ic = dc
+            dc = temp
+
+        dp = [0 for _ in range(len(shorts)+1)]
+        for i in range(1, len(shorts)+1):
+            dp[i] = ic * i
+
+        for i in range(1, len(longs)+1):
+            pre = dp[0]
+            dp[0] = dc * i
+            for j in range(1, len(shorts)+1):
+                temp = dp[j]
+                if longs[i-1] == shorts[j-1]:
+                    dp[j] = pre
+                else:
+                    dp[j] = pre + rc
+                dp[j] = min([dp[j], dp[j-1]+ic])
+                dp[j] = min([dp[j], temp+dc])
+                pre = temp
+
+        return dp[len(shorts)]
+
 
 if __name__ == '__main__':
     str1 = "ab12cd3"
     str2 = "abcdf"
-    print(LowestEditCost.get_lowes_edit_cost_way_1(str1, str2, 5, 3, 2))
+    print(LowestEditCost.get_lowest_edit_cost_way_1(str1, str2, 5, 3, 2))
+    print(LowestEditCost.get_lowest_edit_cost_way_2(str1, str2, 5, 3, 2))
 
     str1 = "abcdf"
     str2 = "ab12cd3"
-    print(LowestEditCost.get_lowes_edit_cost_way_1(str1, str2, 3, 2, 4))
+    print(LowestEditCost.get_lowest_edit_cost_way_1(str1, str2, 3, 2, 4))
+    print(LowestEditCost.get_lowest_edit_cost_way_1(str1, str2, 3, 2, 4))
 
     str1 = ""
     str2 = "ab12cd3"
-    print(LowestEditCost.get_lowes_edit_cost_way_1(str1, str2, 1, 7, 5))
+    print(LowestEditCost.get_lowest_edit_cost_way_1(str1, str2, 1, 7, 5))
+    print(LowestEditCost.get_lowest_edit_cost_way_2(str1, str2, 1, 7, 5))
 
     str1 = "abcdf"
     str2 = ""
-    print(LowestEditCost.get_lowes_edit_cost_way_1(str1, str2, 2, 9, 8))
+    print(LowestEditCost.get_lowest_edit_cost_way_1(str1, str2, 2, 9, 8))
+    print(LowestEditCost.get_lowest_edit_cost_way_2(str1, str2, 2, 9, 8))
