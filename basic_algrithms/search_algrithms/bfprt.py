fbe315baa9013817cb6b92956c4cedfa7a7ba6e7
resolvewang <1796246076@qq.com>
2017-11-30 22:43:19 +0800

    BFPRT部分代码

---
+++ basic_algrithms/search_algrithms/bfprt.py
@@ -0,0 +1,80 @@
+"""
+BFPRT算法
+解决问题：求一个数组中第K大/小的数或者前K小的数。
+时间复杂度：O(N)
+"""
+
+from basic_algrithms.sort_algrithms import insert_sort
+
+
+class BFPRT:
+    @classmethod
+    def get_the_k_number(cls, arr, k):
+        if not arr or len(arr) < k:
+            return
+        cls.select(arr, 0, len(arr)-1, k)
+
+    @classmethod
+    def select(cls, arr, begin, end, k):
+        if begin == end:
+            return arr[begin]
+        priovt = cls.get_median_of_median(arr, begin, end)
+        small, big = cls.patition(arr, begin, end, priovt)
+        if small <= k <= big:
+            return arr[k]
+        elif small > k:
+            return cls.select(arr, begin, small-1, k)
+        else:
+            return cls.select(arr, big+1, end, k)
+
+    @classmethod
+    def get_median_of_median(cls, arr, begin, end):
+        num = end - begin + 1
+        if num % 5 == 0:
+            offset = 0
+        else:
+            offset = 1
+        media_arr = list()
+        for i in range(offset + int(num/5)):
+            begin_index = begin + i * 5
+            end_index = begin_index + 4
+            mid = cls.get_median_value(arr, begin_index, min([end_index, end]))
+            media_arr.append(mid)
+        return cls.get_median_value(media_arr, 0, len(media_arr)-1)
+
+    @classmethod
+    def get_median_value(cls, arr, begin, end):
+        sorted_arr = cls.insert_sort(arr, begin, end)
+        index = int((end - begin) / 2)
+        return sorted_arr[index]
+
+    @classmethod
+    def patition(cls, arr, begin, end, privot):
+        small = begin - 1
+        cur = begin
+        big = end + 1
+        while cur != big:
+            if cur < privot:
+                small += 1
+                arr[small], arr[cur] = arr[cur], arr[small]
+                cur += 1
+            elif cur == privot:
+                cur += 1
+            else:
+                big -= 1
+                arr[big], arr[cur] = arr[cur], arr[big]
+        return small+1, big-1
+
+    @classmethod
+    def insert_sort(cls, arr, begin, end):
+        for i in range(begin+1, end+1):
+            j = i
+            while j > begin and arr[j-1] > arr[j]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+                j -= 1
+
+        return arr
+
+
+if __name__ == '__main__':
+    BFPRT.get_the_k_number([6, 9, 1, 3, 1, 2, 2, 5, 6, 1, 3, 5, 9, 7, 2, 5, 6, 1, 9], 10)
