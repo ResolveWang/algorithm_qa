38d7db4f95241a425ce41a47be216f5f41f5934b
resolvewang <1796246076@qq.com>
2017-12-21 20:32:48 +0800

    找到字符串的最长无重复字符子串

--- string/q18.py
+++ string/q18.py
@@ -13,7 +13,24 @@ str="aabcb"，最长无重复字符子串为"abc"，返回3
 class LongestNotRepeatStr:
     @classmethod
     def get_longest_sut_str(cls, strs):
-        d = set()
-        for i in strs:
-            if i not in d:
-                pass
+        if not strs:
+            return ''
+
+        pre = -1
+        max_length = 0
+        str_map = {i: -1 for i in strs}
+
+        i = 0
+        while i < len(strs):
+            pre = max([pre, str_map[strs[i]]])
+            cur_length = i - pre
+            max_length = max([max_length, cur_length])
+            str_map[strs[i]] = i
+            i += 1
+
+        return max_length
+
+
+if __name__ == '__main__':
+    my_str = 'kqetrpslqrpbbdmjvjba'
+    print(LongestNotRepeatStr.get_longest_sut_str(my_str))
