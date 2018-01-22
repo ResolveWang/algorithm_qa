cc3a4596a4a5e5cf9edcf53fbbd5488ebfd95f85
resolvewang <1796246076@qq.com>
2018-01-22 19:37:46 +0800

    在有序旋转数组中找到最小值

---
+++ other/q19.py
@@ -0,0 +1,44 @@
+"""
+问题描述:有序数组arr可能经过一次旋转,也可能没有,且arr可能存在重复的数,
+例如，有序数组[1,2,3,4,5,6,7],可以旋转处理成[4,5,6,7,1,2,3]等。给定
+一个可能旋转过的有序数组,返回arr的最小值.
+"""
+
+
+class SmallnestNumFinder:
+    @classmethod
+    def get_smallest_num(cls, arr):
+        if not arr:
+            return
+
+        if len(arr) == 1:
+            return arr[0]
+
+        low = 0
+        high = len(arr) - 1
+
+        while True:
+            if arr[low] < arr[high]:
+                return arr[low]
+            mid = int((high - low)/2) + low
+            if mid == low:
+                return arr[high]
+
+            if arr[mid] > arr[high]:
+                # deal mid to high
+                low = mid
+            elif arr[mid] < arr[low]:
+                # deal low to mid
+                high = mid
+            else:
+                cur = low
+                while arr[cur] == arr[high]:
+                    cur += 1
+                if arr[cur] < arr[high]:
+                    return arr[cur]
+                else:
+                    low = cur
+
+
+if __name__ == '__main__':
+    print(SmallnestNumFinder.get_smallest_num([4, 5, 5, 5, 1, 2, 3]))
