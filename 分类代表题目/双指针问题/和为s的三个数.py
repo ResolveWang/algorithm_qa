9d7a50179d1bcf3e9f34bfa14d9c94e9e2770e99
resolvewang <1796246076@qq.com>
2018-04-23 15:23:54 +0800

    和为s的三个数

---
+++ 分类代表题目/双指针问题/和为s的三个数.py
@@ -0,0 +1,31 @@
+"""
+问题: 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+找出所有满足条件且不重复的三元组。
+
+例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，满足要求的三元组集合为：
+[[-1, 0, 1],[-1, -1, 2]]
+"""
+
+
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l +=1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append((nums[i], nums[l], nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
+        return res
