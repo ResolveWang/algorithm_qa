71c0f8e1e945d8ac20194e453255deb0b89db594
resolvewang <1796246076@qq.com>
2018-05-08 18:42:54 +0800

    重复数组中找出唯一不重复的数字

--- 分类代表题目/位运算/重复数组中找出唯一不重复的数字.py
+++ 分类代表题目/位运算/重复数组中找出唯一不重复的数字.py
@@ -14,6 +14,8 @@
 一个通用思路是使用位运算，除了要找的那个元素之外，每个元素出现了k次，那么将元素表示为32位整数，则这些整数
 每个位置相加之后，分别对k取余，则一定能整除，所以算上剩下的那一个元素，剩下的一定是该元素。做法概括起来就是
 统计每一位1的个数，然后对k取余
+
+注意，负数做位运算会有坑。有的时候可以使用n&0x7FFFFFFF将其转换为正数
 """
 
 
@@ -25,10 +27,15 @@ class Solution:
             for j in arr:
                 bit_sum += ((j >> i) & 1)
             res |= ((bit_sum % 3) << i)
-        return res
+        return self.convert(res)
+
+    def convert(self, num):
+        if num >= 2 ** 31:
+            num -= 2**32
+        return num
 
 
 if __name__ == '__main__':
     s = Solution()
-    array = [2, 2, 3, 2]
+    array = [-2]
     print(s.singleNumber(array))
